a6d5c55e4b278151e23525c82a411c53e527ee9a
Benyair Orellana <orellanab@michigan.gov>
2019-04-19 15:49:30 -0400

    home, header and heroku setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// Install express server
+
+const express = requie('express');
+
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files fom the dist diectory
+
+// Replace the '/dist/<to_your_project_name>'
+
+app.use(express.static(__dirname + '.dist/music-portfolio '));
+
+app.get('*', function(req, res) {
+
+    // Replace the '/dist/<to_your_project_name>/index.html'
+    
+    res.sendFile(path.join(__dirname+ '/dist/music-portfolio/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
